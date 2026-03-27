e12cb8a01d0b5fdaddf5f41f5aab69be1784054f
Jemiel <weeabooofficial@gmail.com>
2026-03-28 00:10:16 +0100

    Update panel.js

--- src/commands/admin/panel.js
+++ src/commands/admin/panel.js
@@ -1,4 +1,4 @@
-const { SlashCommandBuilder, PermissionsBitField } = require('discord.js');
+const { SlashCommandBuilder, PermissionFlagsBits } = require('discord.js');
 const { createAdminPanelEmbed } = require('../../utils/embeds');
 const { createAdminPanelButtons } = require('../../utils/buttons');
 const { createErrorEmbed } = require('../../utils/embeds');
@@ -7,10 +7,10 @@ module.exports = {
   data: new SlashCommandBuilder()
     .setName('panel')
     .setDescription('Open the admin control panel')
-    .setDefaultMemberPermissions(PermissionsBitField.Flags.Administrator),
+    .setDefaultMemberPermissions(PermissionFlagsBits.Administrator),
 
-  async execute(interaction) {
-    if (!interaction.member.permissions.has(PermissionsBitField.Flags.Administrator)) {
+async execute(interaction) {
+    if (!interaction.member.permissions.has(PermissionFlagsBits.Administrator)) {
       return interaction.reply({
         embeds: [createErrorEmbed('Permission Denied', 'You must be an administrator to use this command.')],
         ephemeral: true
