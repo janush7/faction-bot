780e7a81a0fcebad7329e91f542ca5fb764c3387
Jemiel <weeabooofficial@gmail.com>
2026-03-28 02:38:46 +0100

    feat: set MWF logo as faction embed thumbnail

--- src/utils/embeds.js
+++ src/utils/embeds.js
@@ -1,14 +1,13 @@
 const { EmbedBuilder } = require('discord.js');
 
-function createFactionEmbed(thumbnailUrl = null) {
-  const embed = new EmbedBuilder()
+const THUMBNAIL_URL = 'https://raw.githubusercontent.com/janush7/faction-bot/main/assets/MWF.png';
+
+function createFactionEmbed() {
+  return new EmbedBuilder()
     .setTitle('Choose your side!')
     .setDescription("Choose the side you'll be playing on by clicking one of the buttons below. After selecting a side, you'll gain access to the channels where the SL briefings will take place. Good luck, and see you on the server!")
-    .setColor(0x011327);
-
-  if (thumbnailUrl) embed.setThumbnail(thumbnailUrl);
-
-  return embed;
+    .setColor(0x011327)
+    .setThumbnail(THUMBNAIL_URL);
 }
 
 function createSuccessEmbed(title, description) {
