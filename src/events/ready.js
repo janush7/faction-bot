a1b8140037107cea3e4ff2269b642230c9b6e604
Jemiel <weeabooofficial@gmail.com>
2026-03-28 02:16:27 +0100

    refactor: src/events/ready.js

--- src/events/ready.js
+++ src/events/ready.js
@@ -5,10 +5,8 @@ module.exports = {
   name: 'ready',
   once: true,
   execute(client) {
-    logger.success(`✅ Bot is online as ${client.user.username}#${client.user.discriminator}`);
-    logger.info(`Bot is in ${client.guilds.cache.size} guild(s)`);
-    
-    // Set bot status
-    client.user.setActivity('Event Management', { type: ActivityType.Listening });
+    logger.success(`Bot is online as ${client.user.username}`);
+    logger.info(`Active in ${client.guilds.cache.size} guild(s)`);
+    client.user.setActivity('Choose your side!', { type: ActivityType.Watching });
   }
 };
