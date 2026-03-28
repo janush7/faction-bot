28503c1bd9a0282b1194e370c4fa622d1ed03651
Jemiel <weeabooofficial@gmail.com>
2026-03-28 04:18:15 +0100

    fix: remove timestamp from faction embed

--- src/utils/embeds.js
+++ src/utils/embeds.js
@@ -7,8 +7,7 @@ function createFactionEmbed() {
     .setTitle('Choose your side!')
     .setDescription("Choose the side you'll be playing on by clicking one of the buttons below. After selecting a side, you'll gain access to the channels where the SL briefings will take place. Good luck, and see you on the server!")
     .setColor(0x011327)
-    .setThumbnail(THUMBNAIL_URL)
-    .setTimestamp();
+    .setThumbnail(THUMBNAIL_URL);
 }
 
 function createSuccessEmbed(title, description) {
